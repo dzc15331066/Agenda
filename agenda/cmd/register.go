7c3e52ded0d4b86e556a821230d351d2ecd08b68
dzc15331066 <2814414634@qq.com>
2017-10-22 12:31:42 +0800

    push cmd-design.md, agenda, testcobra

---
+++ agenda/cmd/register.go
@@ -0,0 +1,63 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// registerCmd represents the register command
+var registerCmd = &cobra.Command{
+	Use:   "register",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		username, _ := cmd.Flags().GetString("user")
+		password, _ := cmd.Flags().GetString("password")
+		email, _ := cmd.Flags().GetString("email")
+		phone, _ := cmd.Flags().GetString("phone")
+		fmt.Println("register!")
+		fmt.Println("username: " + username)
+		fmt.Println("password: " + password)
+		fmt.Println("email: " + email)
+		fmt.Println("phone: " + phone)
+
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(registerCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// registerCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// registerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	registerCmd.Flags().StringP("user", "u", "", "Help message for username")
+	registerCmd.Flags().StringP("password", "", "", "Help message for password")
+	registerCmd.Flags().StringP("email", "", "", "Help message for email")
+	registerCmd.Flags().StringP("phone", "", "", "Help message for phone")
+}
---
+++ cmd-design.md
@@ -0,0 +1,80 @@
+#cmd-design
+
+###列出命令说明
+
+```
+agenda help
+agenda help [command]
+```
+
+### 用户注册
+
+```
+agenda register -uUserName --password pass --email=a@xxx.com --phone=123456
+```
+
+### 用户登录
+
+```
+agenda login -uUserName --password pass
+```
+
+###用户登出
+
+```
+agenda logout
+```
+
+### 用户查询
+
+```
+agenda query
+```
+
+### 用户删除
+
+```
+agenda delUser
+```
+
+### 创建会议
+
+```
+agenda cm --title=meeting --part=participator --start=start_time --end=end_time
+```
+
+### 增删会议参与者
+
+```
+agenda addPart --title=meeting --part=participator
+agenda delPart --title=meeting --part=participator
+```
+
+### 查询会议
+
+```
+agenda qm --start=start_time  --end=end_time
+```
+
+### 取消会议
+
+```
+agenda dm --title=meeting
+```
+
+### 退出会议
+
+```
+agenda em --title=meeting
+```
+
+### 清空会议
+
+```
+agenda clear
+```
+
+
+
+
+
