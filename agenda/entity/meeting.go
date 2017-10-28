369e68fedcb539a815fb0dfb9a004a71bb4a1e25
dzc15331066 <2814414634@qq.com>
2017-10-28 12:42:45 +0800

    简单创建了实体类 User Meeting Storage,待完善

---
+++ agenda/entity/meeting.go
@@ -0,0 +1,22 @@
+package entity
+
+import "time"
+
+type Meeting struct {
+	Sponsor       string
+	Participators []string
+	Start         time.Time
+	End           time.Time
+	Title         string
+}
+
+func NewMeeting(sponsor string, participator string, start time.Time, end time.Time, title string) *Meeting {
+	participators := make([]string, 1)
+	participators = append(participators, participator)
+	return &Meeting{sponsor, participators, start, end, title}
+}
+
+func (m *Meeting) IsParticipator(username string) bool {
+	//add your codes here
+	return true
+}
